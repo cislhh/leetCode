8d5b8a652e244c6c217dc6b344aa184855514fa9
Azo <1353761785@qq.com>
2024-10-21 18:38:46 +0800

    80. 删除有序数组中的重复项 II

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from '@/route/index.js' // 1.引入router
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)    // 2.引用router
+app.mount('#app')
